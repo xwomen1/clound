b2cc154c605e252e3108e1daa6e537d38e92b2a5
Linh <dohoikute@gmail.com>
2023-05-28 08:10:03 +0700

    test

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter your name: ");
+            string name= Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            string age= Console.ReadLine();
+            Console.WriteLine("Hello "+name + " you are "+age );
+            Console.ReadLine();
+        }
+    }
+}
